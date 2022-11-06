0acc065ede48f5a24834e6a200a21985aeae8668
A bit Soul™ <116040643+IvanSvyshch@users.noreply.github.com>
2022-11-06 12:48:27 +0100

    Update array_reverse_order.java

--- array_reverse_order.java
+++ array_reverse_order.java
@@ -6,23 +6,11 @@ package CursorHomeWork.HW2Array;
 public class array_reverse_order {
     public static void main(String[] args) {
         int nums[] = {2, 3, 1, 7, 11};
-        int a, b, t;
-        int size;
+        Arrays.sort(nums);
+        Arrays.toString(nums);
 
-        size = 5;
-
-        for (a = 1; a < size; a++)
-            for (b = size - 1; b >= a; b--) {
-                if (nums[b - 1] > nums[b]) {
-
-                    t = nums[b - 1];
-                    nums[b - 1] = nums[b];
-                    nums[b] = t;
-                }
-            }
         System.out.println("Array Reverse Order");
-        for (int i = size - 1; i >= 0; i--)
+        for (int i = nums.length - 1; i >= 0; i--)
             System.out.print(" " + nums[i]);
     }
 }
-
